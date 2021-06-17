cb53d158fcdd11ae16c6f5e909a3b2c3a9b0aa90
Samyak <samyakdarshan97@gmail.com>
2021-06-18 01:52:36 +0530

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,51 @@
+console.log('Client side JS file is loaded')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+fetch('http://localhost:3000/weather?address=Delhi').then((response) => {
+    response.json().then((data) => {
+    if(data.error){
+        console.log(data.error)
+    } else{
+    console.log(data.location)
+    console.log(data.forecast)
+}
+})
+})
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+//messageOne.textContent ='From JS'
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    //console.log(location)
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    response.json().then((data) => {
+    if(data.error){
+        //console.log(data.error)
+        messageOne.textContent = data.error
+    } else{
+        messageOne.textContent =data.location
+        messageTwo.textContent =data.forecast
+
+    // console.log(data.location)
+    // console.log(data.forecast)
+}
+})
+})
+
+})
---
+++ src/app.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+app.use(express.static(publicDirectoryPath))
+hbs.registerPartials(partialsPath)
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'You are safe',
+        name:'Samyak'
+    })})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        Text: 'This is about page',
+        name: 'Samyak'
+    })})
+
+app.get('/index', (req, res) => {
+    res.render('index', {
+        name: 'Samyak'
+    })})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide a valid address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if(error){
+            return res.send({
+                error
+            })
+        }
+
+        forecast(latitude, longitude,(error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+        })
+    })
+    // res.send({
+    //     place: 'Rourkela',
+    //     weather: 'Its raining',
+    //     address: req.query.address
+    // })
+})
+})
+
+
+app.get('/products',(req,res) => {
+    if(!req.query.search){
+        return res.send({
+            error:'You must provide a search term'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*',(req,res) => {
+    res.render('error404',{
+        errorMessage:'Help Article not found',
+        name:'Samyak'
+    })})
+
+app.get('*', (req, res) => {
+    res.render('error404', {
+        errorMessage: 'Error Page',
+        name: 'Gulu'
+    })})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
